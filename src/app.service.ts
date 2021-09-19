a8e94fc8b4d92feb885bd81b71ca82a030b25111
Elon Aseneka <elonsantos63@gmail.com>
2021-09-19 18:59:58 +0300

    feat(core): Google OAuth2

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,16 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  confirmGoogleOAuth(req) {
+    console.log('req', req);
+    
+    if (!req.user) {
+      return 'No user from google'
+    }
+
+    return {
+      message: 'User information from google',
+      user: req.user
+    }
   }
 }
